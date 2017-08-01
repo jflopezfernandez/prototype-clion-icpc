a13e6a67d1697ce31db329b51119b53e7cf2f7d9
lopezfjose <jflopezfernandez@gmail.com>
2017-08-01 09:20:06 -0400

    Added main header

---
+++ Main.hpp
@@ -0,0 +1,28 @@
+#ifndef PROTOTYPE_MAIN_HPP
+#define PROTOTYPE_MAIN_HPP
+
+
+/** ***** ***** ***** ***** INTEL MODULES ***** ***** ***** ***** ***** */
+#include <mkl.h>
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+#include <cilk/cilk_api_linux.h>
+#include <cilk/reducer.h>
+
+/** ***** ***** ***** ***** C++ STL HEADERS ***** ***** ***** ***** ***** */
+#include <iostream>
+
+/** ***** ***** ***** ***** C LIBRARY HEADERS ***** ***** ***** ***** ***** */
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+
+/** ***** ***** ***** APPLICATION GLOBALS AND SETTINGS ***** ***** ***** ***** */
+//...
+
+/** ***** ***** ***** ***** Application Headers ***** ***** ***** ***** ***** */
+//...
+
+
+#endif //PROTOTYPE_MAIN_HPP
